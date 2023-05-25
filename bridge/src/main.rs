5f884f48f1bda39a91f86fee885aeddcb911b8c1
Ferdia McKeogh <ferdia@mckeogh.tech>
2023-05-25 09:33:48 +0100

    Seeing UART from C++, but only printing one statement

--- bridge/src/main.rs
+++ bridge/src/main.rs
@@ -1,103 +1,135 @@
 #![no_main]
 #![no_std]
 
-use {defmt::error, defmt_rtt as _, rtic::app};
+use {
+    core::mem::MaybeUninit,
+    cortex_m_rt::entry,
+    defmt::{error, info, trace},
+    defmt_rtt as _, stm32f0xx_hal as _,
+    zy12pdn_wrapper::root::{
+        sink_callback,
+        usb_pd::{i2c_bit_bang, mcu_hal, pd_sink},
+    },
+};
 
 mod rgb;
 
-#[app(device = stm32f0xx_hal::pac, peripherals = true, dispatchers = [SPI1])]
-mod app {
-    use core::mem::MaybeUninit;
+#[entry]
+fn main() -> ! {
+    let mut hal = unsafe { MaybeUninit::<mcu_hal>::uninit().assume_init() };
+    unsafe { hal.init() };
+    info!("hal init");
 
-    use {
-        crate::rgb::{Color, Rgb},
-        defmt::{info, trace},
-        stm32f0xx_hal::{
-            gpio::{
-                gpioa::{self, PA5, PA6, PA7},
-                Output, PushPull,
-            },
-            prelude::*,
-        },
-        systick_monotonic::Systick,
-        zy12pdn_wrapper::root::{
-            sink_callback,
-            usb_pd::{i2c_bit_bang, pd_sink},
-        },
-    };
+    let mut i2c = unsafe { MaybeUninit::<i2c_bit_bang>::uninit().assume_init() };
+    unsafe { i2c.init() };
+    trace!("i2c init");
 
-    #[shared]
-    struct Shared {}
+    let mut power_sink = unsafe { MaybeUninit::<pd_sink>::uninit().assume_init() };
+    unsafe { power_sink.set_event_callback(Some(sink_callback)) }
+    trace!("sink callback");
+    unsafe { power_sink.init() };
+    trace!("sink init");
 
-    #[local]
-    struct Local {
-        led: Rgb<PA5<Output<PushPull>>, PA6<Output<PushPull>>, PA7<Output<PushPull>>>,
+    loop {
+        unsafe { power_sink.poll() };
     }
 
-    #[monotonic(binds = SysTick, default = true)]
-    type MonoTimer = Systick<1000>;
-
-    #[init]
-    fn init(cx: init::Context) -> (Shared, Local, init::Monotonics) {
-        let mut flash = cx.device.FLASH;
-        let mut rcc = cx
-            .device
-            .RCC
-            .configure()
-            .sysclk(32u32.mhz())
-            .freeze(&mut flash);
-
-        let mono = Systick::new(cx.core.SYST, rcc.clocks.sysclk().0);
-
-        let gpioa::Parts {
-            pa5,
-            pa6,
-            pa7,
-            pa9,
-            pa10,
-            ..
-        } = cx.device.GPIOA.split(&mut rcc);
-
-        let (pa5, pa6, pa7, sda, scl) = cortex_m::interrupt::free(move |cs| {
-            (
-                pa5.into_push_pull_output(cs),
-                pa6.into_push_pull_output(cs),
-                pa7.into_push_pull_output(cs),
-                pa9.into_open_drain_output(cs),
-                pa10.into_push_pull_output(cs),
-            )
-        });
-
-        let mut led = Rgb::new(pa5, pa6, pa7);
-        led.set(Color::White);
-
-        info!("init done");
-
-        let mut i2c = unsafe { MaybeUninit::<i2c_bit_bang>::uninit().assume_init() };
-        unsafe { i2c.init() };
-
-        trace!("i2c init");
-
-        let mut power_sink = unsafe { MaybeUninit::<pd_sink>::uninit().assume_init() };
-        unsafe { power_sink.set_event_callback(Some(sink_callback)) }
-        trace!("sink calback");
-        unsafe { power_sink.init() };
-        trace!("i2c init");
-
-        loop {
-            unsafe { power_sink.poll() };
-        }
-
-        (Shared {}, Local { led }, init::Monotonics(mono))
-    }
-
-    #[idle(local = [led])]
-    fn idle(cx: idle::Context) -> ! {
-        loop {
-            //cx.local.pd.poll(monotonics::now());
-        }
-    }
+    panic!();
 }
+
+// #[app(device = stm32f0xx_hal::pac, peripherals = true, dispatchers = [SPI1])]
+// mod app {
+//     use core::mem::MaybeUninit;
+
+//     use {
+//         crate::rgb::{Color, Rgb},
+//         defmt::{info, trace},
+//         stm32f0xx_hal::{
+//             gpio::{
+//                 gpioa::{self, PA5, PA6, PA7},
+//                 Output, PushPull,
+//             },
+//             prelude::*,
+//         },
+//         systick_monotonic::Systick,
+//         zy12pdn_wrapper::root::{
+//             sink_callback,
+//             usb_pd::{i2c_bit_bang, pd_sink},
+//         },
+//     };
+
+//     #[shared]
+//     struct Shared {}
+
+//     #[local]
+//     struct Local {
+//         led: Rgb<PA5<Output<PushPull>>, PA6<Output<PushPull>>, PA7<Output<PushPull>>>,
+//     }
+
+//     #[monotonic(binds = SysTick, default = true)]
+//     type MonoTimer = Systick<1000>;
+
+//     #[init]
+//     fn init(cx: init::Context) -> (Shared, Local, init::Monotonics) {
+//         let mut flash = cx.device.FLASH;
+//         let mut rcc = cx
+//             .device
+//             .RCC
+//             .configure()
+//             .sysclk(32u32.mhz())
+//             .freeze(&mut flash);
+
+//         let mono = Systick::new(cx.core.SYST, rcc.clocks.sysclk().0);
+
+//         let gpioa::Parts {
+//             pa5,
+//             pa6,
+//             pa7,
+//             pa9,
+//             pa10,
+//             ..
+//         } = cx.device.GPIOA.split(&mut rcc);
+
+//         let (pa5, pa6, pa7, sda, scl) = cortex_m::interrupt::free(move |cs| {
+//             (
+//                 pa5.into_push_pull_output(cs),
+//                 pa6.into_push_pull_output(cs),
+//                 pa7.into_push_pull_output(cs),
+//                 pa9.into_open_drain_output(cs),
+//                 pa10.into_push_pull_output(cs),
+//             )
+//         });
+
+//         let mut led = Rgb::new(pa5, pa6, pa7);
+//         led.set(Color::White);
+
+//         info!("init done");
+
+//         let mut i2c = unsafe { MaybeUninit::<i2c_bit_bang>::uninit().assume_init() };
+//         unsafe { i2c.init() };
+
+//         trace!("i2c init");
+
+//         let mut power_sink = unsafe { MaybeUninit::<pd_sink>::uninit().assume_init() };
+//         unsafe { power_sink.set_event_callback(Some(sink_callback)) }
+//         trace!("sink calback");
+//         unsafe { power_sink.init() };
+//         trace!("i2c init");
+
+//         loop {
+//             unsafe { power_sink.poll() };
+//         }
+
+//         (Shared {}, Local { led }, init::Monotonics(mono))
+//     }
+
+//     #[idle(local = [led])]
+//     fn idle(cx: idle::Context) -> ! {
+//         loop {
+//             //cx.local.pd.poll(monotonics::now());
+//         }
+//     }
+// }
 
 #[panic_handler]
 fn panic_handler(_: &core::panic::PanicInfo) -> ! {
--- zy12pdn-wrapper/wrapper.hpp
+++ zy12pdn-wrapper/wrapper.hpp
@@ -10,60 +10,37 @@ static i2c_bit_bang i2c;
 //
 void sink_callback(callback_event event);
 
-// int main() {
-//     hal.init();
-
-//     DEBUG_LOG("Saved mode: %d\r\n", desired_mode);
-
-//     power_sink.set_event_callback(sink_callback);
-//     power_sink.init();
-
-//     // Work in regular loop
-//     while (true) {
-//         power_sink.poll();
-//     }
-// }
-
-// Called when the source advertises new capabilities
-// Be careful with debug output. If one of the capbilities is not
-// requested in time, the power suplly will reset.
-void on_source_caps_changed()
-{
-    int voltage = 5000;
-
-    // Take maximum voltage
-    for (int i = 0; i < power_sink.num_source_caps; i++)
-    {
-        if (power_sink.source_caps[i].voltage > voltage)
-        {
-            voltage = power_sink.source_caps[i].voltage;
-        }
-    }
-
-    // Limit voltage to 20V as the voltage regulator was likely selected to handle 20V max
-    if (voltage > 20000)
-        voltage = 20000;
-
-    power_sink.request_power(voltage);
-}
-
 // Called when the USB PD controller triggers an event
 void sink_callback(callback_event event)
 {
+    int voltage = 5000;
 
     switch (event)
     {
     case callback_event::source_caps_changed:
 
-        on_source_caps_changed();
+        // Take maximum voltage
+        for (int i = 0; i < power_sink.num_source_caps; i++)
+        {
+            if (power_sink.source_caps[i].voltage > voltage)
+            {
+                voltage = power_sink.source_caps[i].voltage;
+            }
+        }
+
+        // Limit voltage to 20V as the voltage regulator was likely selected to handle 20V max
+        if (voltage > 20000)
+        {
+            voltage = 20000;
+        }
+
+        power_sink.request_power(voltage);
         break;
 
     case callback_event::power_ready:
-
         break;
 
     case callback_event::protocol_changed:
-
         break;
 
     default:
