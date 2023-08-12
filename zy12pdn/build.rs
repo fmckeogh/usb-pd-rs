0e073daefde48cf915ddc91bf45b71cb25a3af22
Ferdia McKeogh <ferdia@mckeogh.tech>
2023-08-12 18:42:33 +0100

    Move message ID counter to `State` enum

--- zy12pdn/build.rs
+++ zy12pdn/build.rs
@@ -8,7 +8,8 @@
 //! updating `memory.x` ensures a rebuild of the application with the
 //! new memory settings.
 //!
-//! The build script also sets the linker flags to tell it which link script to use.
+//! The build script also sets the linker flags to tell it which link script to
+//! use.
 
 use std::{env, fs::File, io::Write, path::PathBuf};
 
@@ -31,8 +32,8 @@ fn main() {
     // Specify linker arguments.
     println!("cargo:rustc-linker=flip-link");
 
-    // `--nmagic` is required if memory section addresses are not aligned to 0x10000,
-    // for example the FLASH and RAM sections in your `memory.x`.
+    // `--nmagic` is required if memory section addresses are not aligned to
+    // 0x10000, for example the FLASH and RAM sections in your `memory.x`.
     // See https://github.com/rust-embedded/cortex-m-quickstart/pull/95
     // println!("cargo:rustc-link-arg=--nmagic");
 
