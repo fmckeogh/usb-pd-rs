820bbb4f2451ce1a2c9bda24779d97e15ec9cb30
okhsunrog <me@okhsunrog.ru>
2024-06-18 16:10:18 +0300

    Switch to `heapless`

--- usb-pd/src/message.rs
+++ usb-pd/src/message.rs
@@ -9,14 +9,15 @@ use {
     },
     byteorder::{ByteOrder, LittleEndian},
     defmt::{warn, Format},
+    heapless::Vec,
 };
 
-#[derive(Clone, Copy, Format)]
+#[derive(Clone, Format)]
 pub enum Message {
     Accept,
     Reject,
     Ready,
-    SourceCapabilities([Option<PowerDataObject>; 10]),
+    SourceCapabilities(Vec<PowerDataObject, 10>),
     Unknown,
 }
 
@@ -27,7 +28,28 @@ impl Message {
             MessageType::Control(ControlMessageType::Reject) => Message::Reject,
             MessageType::Control(ControlMessageType::PsRdy) => Message::Ready,
             MessageType::Data(DataMessageType::SourceCapabilities) => Message::SourceCapabilities(
-                parse_source_capabilities(header.num_objects() as usize, payload),
+                payload
+                    .chunks_exact(4)
+                    .take(header.num_objects() as usize)
+                    .map(|buf| PowerDataObjectRaw(LittleEndian::read_u32(buf)))
+                    .map(|pdo| match pdo.kind() {
+                        0b00 => PowerDataObject::FixedSupply(FixedSupply(pdo.0)),
+                        0b01 => PowerDataObject::Battery(Battery(pdo.0)),
+                        0b10 => PowerDataObject::VariableSupply(VariableSupply(pdo.0)),
+                        0b11 => PowerDataObject::AugmentedPowerDataObject({
+                            match AugmentedPowerDataObjectRaw(pdo.0).supply() {
+                                0b00 => {
+                                    AugmentedPowerDataObject::SPR(SPRProgrammablePowerSupply(pdo.0))
+                                }
+                                0b01 => {
+                                    AugmentedPowerDataObject::EPR(EPRAdjustableVoltageSupply(pdo.0))
+                                }
+                                _ => unreachable!(),
+                            }
+                        }),
+                        _ => unreachable!(),
+                    })
+                    .collect(),
             ),
             _ => {
                 warn!("unknown message type");
@@ -35,29 +57,4 @@ impl Message {
             }
         }
     }
-}
-
-fn parse_source_capabilities(num_objects: usize, payload: &[u8]) -> [Option<PowerDataObject>; 10] {
-    let mut capabilities = [None; 10];
-    payload
-        .chunks_exact(4)
-        .take(num_objects)
-        .map(|buf| PowerDataObjectRaw(LittleEndian::read_u32(buf)))
-        .map(|pdo| match pdo.kind() {
-            0b00 => PowerDataObject::FixedSupply(FixedSupply(pdo.0)),
-            0b01 => PowerDataObject::Battery(Battery(pdo.0)),
-            0b10 => PowerDataObject::VariableSupply(VariableSupply(pdo.0)),
-            0b11 => PowerDataObject::AugmentedPowerDataObject({
-                match AugmentedPowerDataObjectRaw(pdo.0).supply() {
-                    0b00 => AugmentedPowerDataObject::SPR(SPRProgrammablePowerSupply(pdo.0)),
-                    0b01 => AugmentedPowerDataObject::EPR(EPRAdjustableVoltageSupply(pdo.0)),
-                    _ => unreachable!(),
-                }
-            }),
-            _ => unreachable!(),
-        })
-        .enumerate()
-        .for_each(|(i, pdo)| capabilities[i] = Some(pdo));
-
-    capabilities
 }
